b60e1732c7828f0cb621fd06053534dc734e702f
Andrew Rumiantsev <andy1116286@gmail.com>
2017-03-10 21:54:43 +0300

    Added tag picker
    Some things need to be added

--- graphics.js
+++ graphics.js
@@ -25,7 +25,29 @@ var mouseEvent= function (evnt) {
         document.getElementById("pageSigns").style.left=globalSignsPosition+"px";
 
 };
-
+var mouseOverBars=function (evnt) {
+    document.getElementById("popoverWindow").style.visibility="visible";
+    var objectsToBlur = document.getElementsByClassName("blurred-object");
+    for(var i=0;i<objectsToBlur.length;i++)
+    {
+        objectsToBlur[i].style.filter="blur(3px)";
+        objectsToBlur[i].style.pointerEvents="none";
+    }
+    console.log("Will show popover");
+};
+var mouseNotOverBars=function (evnt) {
+    document.getElementById("popoverWindow").style.visibility="hidden";
+    var objectsToBlur = document.getElementsByClassName("blurred-object");
+    for(var i=0;i<objectsToBlur.length;i++)
+    {
+        objectsToBlur[i].style.filter="none";
+        objectsToBlur[i].style.pointerEvents="all";
+    }
+    console.log("Will hide popover");
+};
 document.body.addEventListener("wheel",mouseEvent);
+document.getElementById("bars-picker").addEventListener("mouseover",mouseOverBars);
+document.getElementById("popoverWindow").addEventListener("mouseleave",mouseNotOverBars);
+
 // console.log("position:"+parseInt(document.getElementById("pageRope0").style.left,10));
 // document.getElementById("pageSigns").style.top=document.getElementById("pageRope0").style.top;
